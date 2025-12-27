1b21d4cf037a7bdeb03a0f5adfac33030dc18307
zamir <mayzamir77@gmail.com>
2025-12-27 18:51:08 +0200

    may changes

--- main.py
+++ main.py
@@ -20,10 +20,21 @@ Session(app)
 def invalid_route(e):
     return redirect("/")
 
-@app.route('/')
-def homepage():## נעשה ניתובים לדף הבית הרלוונטי לפי סשנים
-    return render_template('homepage.html')
 
+@app.route('/')
+def homepage():
+    user_type = session.get('user_type')
+    origins = get_flights_origins()
+    destinations = get_flights_destinations()
+    today_str = date.today().isoformat()
+
+    if user_type == 'manager':
+        return render_template('manager_homepage.html', name=session.get('user_first_name'))
+
+    elif user_type == 'customer':
+        return render_template('customer_homepage.html', name=session.get('user_first_name'),origins=origins, destinations=destinations, today=today_str)
+
+    return render_template('homepage.html',origins=origins, destinations=destinations,today=today_str)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -36,13 +47,13 @@ def login():
     if not customer_exists(email):
         return render_template(
             'login.html',
-            error="User not found. Please register or place an order as an unregistered customer."
+            error="User not found. Please register or place an order as a guest."
         )
 
     if not registered_customer_exists(email):
         return render_template(
             'login.html',
-            error="This email belongs to an unregistered customer. Please register to log in."
+            error="This email belongs to a guest. Please register to log in."
         )
 
     if not check_password_customer(email, password):
@@ -52,14 +63,24 @@ def login():
         )
 
     else:
+        session['user_email'] = email
+        session['user_type'] = 'customer'
+        session['user_first_name'] = get_customer_details(email)[0]
+        session['user_last_name'] = get_customer_details(email)[1]
+        session['user_passport'] = get_customer_details(email)[2]
+        session['user_birth_date'] = get_customer_details(email)[3]
         return redirect('/')
 
-
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'GET':
-        return render_template('signup.html')
+        today_str = date.today().isoformat()
+        return render_template('signup.html',today=today_str)
 
     email = request.form.get('email')
     passport_number=request.form.get('passport_number')
@@ -67,17 +88,139 @@ def signup():
     password = request.form.get('password')
     first_name=request.form.get('first_name')
     last_name=request.form.get('last_name')
+    raw_phones= request.form.getlist('phones')
+    phones = [p for p in raw_phones if p.strip()]
+
 
     if registered_customer_exists(email):
-        return render_template('signup.html', error="This email is taken")
+        return render_template('signup.html', error="This email is taken",today=date.today().isoformat())
 
     if customer_exists(email):
-        turn_into_registered_db(email,first_name,last_name,passport_number,birth_date,date.today(),password)
+        turn_into_registered_db(email,first_name,last_name,passport_number,birth_date,date.today(),password,phones)
+        session['user_email'] = email
+        session['user_type'] = 'customer'
+        session['user_first_name'] = get_customer_details(email)[0]
+        session['user_last_name'] = get_customer_details(email)[1]
+        session['user_passport'] = get_customer_details(email)[2]
+        session['user_birth_date'] = get_customer_details(email)[3]
         return redirect('/')
 
     else:
-        add_customer_to_db(email,first_name,last_name,passport_number,birth_date,date.today(),password)
+        add_customer_to_db(email,first_name,last_name,passport_number,birth_date,date.today(),password, phones)
+        session['user_email'] = email
+        session['user_type'] = 'customer'
+        session['user_first_name'] = get_customer_details(email)[0]
+        session['user_last_name'] = get_customer_details(email)[1]
+        session['user_passport'] = get_customer_details(email)[2]
+        session['user_birth_date'] = get_customer_details(email)[3]
         return redirect('/')
+
+
+
+@app.route('/search_flights', methods=['POST'])
+def search_flights():
+    flight_date= request.form.get('flight_date')
+    origin = request.form.get('origin')
+    destination=request.form.get('destination')
+    num_seats = request.form.get('num_seats')
+
+    result = get_relevant_flights(flight_date,origin, destination, num_seats)
+    if not result:
+        return render_template('homepage.html', origins=get_flights_origins(),
+                               destinations=get_flights_destinations(),today=date.today().isoformat(), error='No available flights. Please select differently')
+    return render_template('show_flights.html', flights=result, requested_seats=num_seats)
+
+
+
+@app.route('/select_seats', methods=['POST'])
+def select_seats():
+    selected_data = request.form.get('selected_option')
+    num_seats = int(request.form.get('num_seats'))
+    flight_id, class_type = selected_data.split(',')
+    price_per_seat = get_price_for_class(flight_id, class_type)
+
+    session['booking_data'] = {
+        'flight_id': flight_id,
+        'class_type': class_type,
+        'num_seats': num_seats,
+        'price_per_seat': price_per_seat,
+        'total_price': price_per_seat * num_seats
+    }
+
+    class_info = get_class_layout(flight_id, class_type)
+    vacant_seats = get_vacant_seats(flight_id)
+    vacant_set = {(s[1], s[2]) for s in vacant_seats if s[0] == class_type}
+    return render_template('seat_selection.html',flight_id=flight_id, class_type=class_type, layout=class_info,vacant_set=vacant_set, num_seats=num_seats)
+
+@app.route('/validate_seats', methods=['POST'])
+def validate_seats():
+    selected_seats=request.form.getlist("selected_seats")
+    booking = session.get('booking_data')
+
+    if len(selected_seats) != booking['num_seats']:
+        class_info = get_class_layout(booking['flight_id'], booking['class_type'])
+        vacant_seats = get_vacant_seats(booking['flight_id'])
+        vacant_set = {(s[1], s[2]) for s in vacant_seats if s[0] == booking['class_type']}
+        return render_template('seat_selection.html',
+                               flight_id=booking['flight_id'],
+                               class_type=booking['class_type'],
+                               layout=class_info,
+                               vacant_set=vacant_set,
+                               num_seats=booking['num_seats'],
+                               error=f"Choose exactly {booking['num_seats']} Seats!")
+
+    else:
+        booking['selected_seats'] = selected_seats
+        session['booking_data'] = booking
+        return render_template("passenger_details.html", seats=selected_seats, today=date.today().isoformat())
+
+
+@app.route('/review_order', methods=['POST'])
+def review_order():
+    booking = session.get('booking_data')
+    if not booking:
+        return redirect('/')
+
+    booking['first_name'] = request.form.get('first_name')
+    booking['last_name'] = request.form.get('last_name')
+    booking['email'] = request.form.get('email')
+    booking['passport'] = request.form.get('passport')
+    booking['birth_date'] = request.form.get('birth_date')
+
+    session['booking_data'] = booking
+
+    return render_template('order_summary.html', data=booking)
+
+@app.route('/confirm_booking', methods=['POST'])
+def confirm_booking():
+    pass
+
+
+# --- ממשק לקוח / אורח ---
+
+
+@app.route('/manage_booking', methods=['GET', 'POST'])
+def manage_booking():
+    # ניהול הזמנה לפי מספר הזמנה ואימייל [cite: 14, 20, 129]
+    pass
+
+@app.route('/booking_history')
+def booking_history():
+    # צפייה בהיסטוריית הזמנות למשתמשים רשומים בלבד [cite: 66, 71, 158]
+    pass
+
+
+@app.route('/cancel_booking_request', methods=['POST'])
+def cancel_booking_request():
+    # עמוד ביטול הזמנה ובדיקת תנאי 36 שעות [cite: 134, 138]
+    pass
+
+@app.route('/booking_cancelled')
+def booking_cancelled():
+    # הצגת פרטי הזמנה שבוטלה בהצלחה [cite: 151, 152]
+    pass
+
+# --- ממשק מנהל (Admin) ---
 
 @app.route('/adminlogin', methods=['GET', 'POST'])
 def adminlogin():
@@ -95,97 +238,6 @@ def adminlogin():
 
     else:
         return redirect('/')
-
-##נעביר להומפייג את כל הממשק של סרצ פלייטס
-@app.route('/search_flights', methods=['GET', 'POST'])
-def search_flights():
-    if request.method == 'GET':
-        return render_template('search_flights.html', origins= get_flights_origins(), destinations=get_flights_destinations())
-
-    date= request.form.get('flight_date')
-    origin = request.form.get('origin')
-    destination=request.form.get('destination')
-
-    result = get_relevant_flights(date,origin, destination)
-    if not result:
-        return render_template('search_flights.html', origins=get_flights_origins(),
-                               destinations=get_flights_destinations(), error='No available flights. Please select differently')
-    return render_template('show_flights.html', flights= result)
-
-
-##הולך להיות שואו פלייטס שמראה את כל הטיסות הרלוונטיות
-@app.route('/select_flight', methods=['POST'])
-def select_flight():
-    flight_id = request.form.get('flight_id')
-
-    classes = class_in_plane(flight_id)
-
-    if len(classes) == 1:
-        pass
-
-    else:
-        pass
-
-
-@app.route('/select_seats_economy')
-def select_seats_economy():
-    # בחירת מושבים למחלקת תיירים [cite: 84, 88]
-    pass
-
-
-@app.route('/select_seats_business')
-def select_seats_business():
-    # בחירת מושבים למחלקת עסקים [cite: 85, 99]
-    pass
-
-@app.route('/passenger_details')
-def passenger_details():
-    # להוסיף / מתמלא אוטומטית פרטי הנוסעים
-    #להוסיף לפה אם הגענו בפוסט אז לשים את פרטי ההזמנה הסופיים
-    pass
-
-
-# --- ממשק לקוח / אורח ---
-
-@app.route('/logout')
-def logout():
-    # ניתוב לדף הבית + מחיקת סשן [cite: 69, 73]
-    pass
-
-@app.route('/manage_booking', methods=['GET', 'POST'])
-def manage_booking():
-    # ניהול הזמנה לפי מספר הזמנה ואימייל [cite: 14, 20, 129]
-    pass
-
-@app.route('/booking_history')
-def booking_history():
-    # צפייה בהיסטוריית הזמנות למשתמשים רשומים בלבד [cite: 66, 71, 158]
-    pass
-
-
-
-
-@app.route('/order_summary', methods=['GET', 'POST'])
-def order_summary():
-    # סיכום הזמנה ומילוי פרטי נוסע [cite: 96, 110, 111]
-    pass
-
-@app.route('/confirm_order')
-def confirm_order():
-    # דף אישור סופי לאחר ביצוע הזמנה ועדכון DB [cite: 117, 119]
-    pass
-
-@app.route('/cancel_booking_request', methods=['POST'])
-def cancel_booking_request():
-    # עמוד ביטול הזמנה ובדיקת תנאי 36 שעות [cite: 134, 138]
-    pass
-
-@app.route('/booking_cancelled')
-def booking_cancelled():
-    # הצגת פרטי הזמנה שבוטלה בהצלחה [cite: 151, 152]
-    pass
-
-# --- ממשק מנהל (Admin) ---
 
 @app.route('/admin_homepage')
 def admin_dashboard():
--- utils.py
+++ utils.py
@@ -25,7 +25,21 @@ def db_cur():
         if mydb:
             mydb.close()
 
+class FlightResult:
+    def __init__(self, data_tuple):
+        self.id = data_tuple[0]
+        self.departure_time = data_tuple[1]
+        self.departure_date = data_tuple[2]
+        self.aircraft_id = data_tuple[3]
+        self.duration = data_tuple[4]
+        self.class_type = data_tuple[5]
+        self.price = data_tuple[6]
+        self.arrival_date = data_tuple[7]
+        self.arrival_time = data_tuple[8]
+        self.origin=data_tuple[9]
+        self.destination=data_tuple[10]
 
+##login checks
 def customer_exists(email):
     with db_cur() as cursor:
         cursor.execute("SELECT 1 FROM Customer WHERE email = %s", (email,))
@@ -44,17 +58,29 @@ def check_password_customer(email, password):
         result = cursor.fetchone()
         return result is not None
 
-def turn_into_registered_db(email,first_name,last_name,passport_number,birth_date,registration_date,password):
+def get_customer_details(email):
+    with db_cur() as cursor:
+        cursor.execute("SELECT first_name_english, last_name_english,passport_number,birth_date FROM Customer as c join registered_customer as rc on c.email=rc.email WHERE c.email = %s", (email,))
+        result = cursor.fetchone()
+        if result:
+            return result
+
+##signup func
+def turn_into_registered_db(email,first_name,last_name,passport_number,birth_date,registration_date,password, phones):
     with db_cur() as cursor:
         cursor.execute("UPDATE Customer SET first_name_english = %s, last_name_english = %s WHERE email = %s",(first_name, last_name, email))
         cursor.execute("DELETE FROM Unregistered_Customer WHERE email= %s",(email,))
         cursor.execute("INSERT INTO Registered_Customer VALUES (%s,%s,%s,%s,%s)", (email,passport_number,birth_date,registration_date,password))
+        for p in phones:
+            cursor.execute("INSERT IGNORE INTO customer_phone_numbers VALUES(%s,%s)",(email,p))
 
-def add_customer_to_db(email,first_name,last_name,passport_number,birth_date,registration_date,password):
+def add_customer_to_db(email,first_name,last_name,passport_number,birth_date,registration_date,password, phones):
     with db_cur() as cursor:
         cursor.execute("INSERT INTO Customer VALUES (%s,%s,%s)", (email,first_name,last_name))
         cursor.execute("INSERT INTO Registered_Customer VALUES (%s,%s,%s,%s,%s)",
                        (email, passport_number, birth_date, registration_date, password))
+        for p in phones:
+            cursor.execute("INSERT INTO customer_phone_numbers VALUES(%s,%s)",(email,p))
 
 
 def admin_exists(id):
@@ -69,44 +95,95 @@ def check_password_manager(id,password):
         result = cursor.fetchone()
         return result is not None
 
+##search flight func
 def get_flights_origins():
     with db_cur() as cursor:
-        cursor.execute("SELECT DISTINCT origin_airport FROM Flight")
+        cursor.execute("SELECT DISTINCT origin_airport FROM routes")
         result = cursor.fetchall()
         return result
 
 def get_flights_destinations():
     with db_cur() as cursor:
-        cursor.execute("SELECT DISTINCT destination_airport FROM Flight")
+        cursor.execute("SELECT DISTINCT destination_airport FROM routes")
         result = cursor.fetchall()
         return result
 
-def get_relevant_flights(date, origin, destination):
+def get_vacant_seats(flight_id):
     with db_cur() as cursor:
-        cursor.execute( ''' SELECT flight_id, flight_duration, departure_time, departure_date, origin_airport, destination_airport, aircraft_id,
-            DATE(
-                    ADDTIME(
-                        TIMESTAMP(departure_date, departure_time),
-                        SEC_TO_TIME(flight_duration * 60)
-                    )
-                ) AS arrival_date,
-
-                TIME(
-                    ADDTIME(
-                        TIMESTAMP(departure_date, departure_time),
-                        SEC_TO_TIME(flight_duration * 60)
-                    )
-                ) AS arrival_time
-
-            FROM Flight
-            WHERE departure_date = %s AND origin_airport = %s AND destination_airport = %s AND flight_status = %s''',
-            (date, origin, destination, 'Active'))
+        query = '''
+            SELECT s.class_type, s.row_num, s.column_letter
+            FROM seat s
+            JOIN flight f ON s.aircraft_id = f.aircraft_id
+            WHERE f.flight_id = %s
+            AND (s.row_num, s.column_letter, s.class_type) NOT IN (
+                SELECT ss.row_num, ss.column_letter, ss.class_type
+                FROM selected_seats_in_booking ss
+                JOIN booking b ON ss.booking_id = b.booking_id
+                WHERE b.flight_id = %s AND b.booking_status = 'Active'
+            )
+        '''
+        cursor.execute(query, (flight_id, flight_id))
         return cursor.fetchall()
 
-def class_in_plane(aircraft_id):
+
+def get_relevant_flights(date, origin, destination, requested_seats):
     with db_cur() as cursor:
-        cursor.execute('SELECT class_type FROM class WHERE aircraft_id=%s', (aircraft_id,))
-        return [row[0] for row in cursor.fetchall()]
+        query = '''
+            SELECT f.flight_id, f.departure_time, f.departure_date, f.aircraft_id,
+                   r.flight_duration_mins, cif.class_type, cif.seat_price,
+                   DATE(ADDTIME(TIMESTAMP(f.departure_date, f.departure_time), SEC_TO_TIME(r.flight_duration_mins * 60))) AS arrival_date,
+                   TIME(ADDTIME(TIMESTAMP(f.departure_date, f.departure_time), SEC_TO_TIME(r.flight_duration_mins * 60))) AS arrival_time,
+                   f.origin_airport, f.destination_airport
+            FROM flight f
+            JOIN routes r ON f.origin_airport = r.origin_airport AND f.destination_airport = r.destination_airport
+            JOIN classes_in_flight cif ON f.flight_id = cif.flight_id
+            WHERE f.departure_date = %s 
+              AND f.origin_airport = %s 
+              AND f.destination_airport = %s 
+              AND f.flight_status = 'Active'
+        '''
+        cursor.execute(query, (date, origin, destination))
+        all_options = cursor.fetchall()
+
+        final_results = []
+        for opt in all_options:
+            flight_id = opt[0]
+            class_type = opt[5]
+
+            vacant_seats = get_vacant_seats(flight_id)
+            class_vacant = [s for s in vacant_seats if s[0] == class_type]
+
+            if len(class_vacant) >= int(requested_seats):
+                final_results.append(opt)
+        final_objects = []
+        for opt in final_results:
+            final_objects.append(FlightResult(opt))
+
+        return final_objects
+
+def get_price_for_class(flight_id, class_type):
+    with db_cur() as cursor:
+        cursor.execute("SELECT seat_price from classes_in_flight WHERE flight_id =%s and class_type=%s",(flight_id,class_type))
+        result = cursor.fetchone()
+        if result:
+            return result[0]
+        return 0
+
+
+
+
+def get_class_layout(flight_id, class_type):
+    with db_cur() as cursor:
+        query = """
+            SELECT c.num_rows, c.num_columns, (SELECT min(row_num) 
+            FROM seat s join flight f2 on f2.aircraft_id=s.aircraft_id
+            WHERE f2.flight_id=%s and s.class_type= %s)
+            FROM class c
+            JOIN flight f ON c.aircraft_id = f.aircraft_id
+            WHERE f.flight_id = %s AND c.class_type = %s
+        """
+        cursor.execute(query, (flight_id, class_type, flight_id, class_type))
+        return cursor.fetchone()
 
 
 
